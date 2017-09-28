782f7e70ead0e39814460fad4090660984cdc2b8
Will Russell <russellgwill@gmail.com>
2017-09-28 18:49:59 +0100

    Commiting Updates

--- js/controller.js
+++ js/controller.js
@@ -2,7 +2,29 @@ angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
         $scope.title = 'Welcome to the SnapCount';
     })
+    .controller('NewsController', function($scope) {
+        $scope.news = 'Latest News';
+    
+        $scope.showMe = false;
+        $scope.myFunc = function() {
+            $scope.showMe = !$scope.showMe;
+        }
+    })
+    .controller('MediaController', function($scope) {
+        $scope.media = 'This is the locker room';
+    
+        $scope.showMe = false;
+        $scope.myFunc = function() {
+            $scope.showMe = !$scope.showMe;
+        }
+    })
+    .controller('TeamController', function($scope) {
+        $scope.team = 'The Podcast Team Members';
+    })
     .controller('ContactController', function($scope) {
+        
+        $scope.contact = 'Please Register';
+    
         $scope.registrationUser = {};
         
         $scope.submitForm = function() {
