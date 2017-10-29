6b7df6ff14c3c7ab47ac1f39ebe8c5b88ed4679b
Will Russell <russellgwill@gmail.com>
2017-10-29 22:23:55 +0000

    Committing Updates

--- js/app.js
+++ js/app.js
@@ -2,25 +2,20 @@ angular.module('SnapCount', ['ngRoute', 'RouteControllers', 'ngAnimate']);
 
 angular.module('SnapCount').config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true); //Enables href routing without hashes
-    
     $routeProvider.when('/', {
-        templateUrl: 'templates/home.html',
-        controller: 'HomeController'
-    })
-    .when('/news', {
-        templateUrl: 'templates/news.html',
-        controller: 'NewsController'
-    })
-    .when('/media', {
-        templateUrl: 'templates/media.html',
-        controller: 'MediaController'
-    })
-    .when('/team', {
-        templateUrl: 'templates/team.html',
-        controller: 'TeamController'
-    })
-    .when('/contact', {
-        templateUrl: 'templates/contact.html',
-        controller: 'ContactController'
+          templateUrl: 'templates/home.html'
+        , controller: 'HomeController'
+    }).when('/news', {
+          templateUrl: 'templates/news.html'
+        , controller: 'NewsController'
+    }).when('/media', {
+          templateUrl: 'templates/media.html'
+        , controller: 'MediaController'
+    }).when('/team', {
+          templateUrl: 'templates/team.html'
+        , controller: 'TeamController'
+    }).when('/contact', {
+          templateUrl: 'templates/contact.html'
+        , controller: 'ContactController'
     });
 });
